e7201d89eb48c0d31d4dd7b86496bbd9a05ac89d
Maltaa7 <martajakubowskakra@gmail.com>
2022-11-30 21:18:56 +0100

    Dao read user

--- TaskManager/src/main/java/pl_coderslab/entity/MainDao.java
+++ TaskManager/src/main/java/pl_coderslab/entity/MainDao.java
@@ -2,5 +2,20 @@ package pl_coderslab.entity;
 
 public class MainDao {
 
+    public static void main(String[] args) {
+        UserDao userDao1 = new UserDao();
+        User user1 = new User();
+        user1 = userDao1.read(4);
+        System.out.println(user1.getUserName());
+        System.out.println(user1.getPassword());
+        System.out.println(user1.getEmail());
 
+        UserDao userDao2 = new UserDao();
+        User user2 = new User();
+        user2 = userDao2.read(18);
+        if (userDao2.read(18) == null) {
+            System.out.println("Nie istnieje user o id 18");
+        }
+
+    }
 }
--- TaskManager/src/main/java/pl_coderslab/entity/UserDao.java
+++ TaskManager/src/main/java/pl_coderslab/entity/UserDao.java
@@ -1,8 +1,32 @@
 package pl_coderslab.entity;
 
 
+import pl_coderslab.DbUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class UserDao {
-
+    private static final String READ_USER_QUERY = "SELECT email, username, password FROM users WHERE id = ?";
+    public User read(int userId) {
+        User user = new User();
+        user.setId(userId);
+        try (Connection connection = DbUtil.connect();
+             PreparedStatement preStmt = connection.prepareStatement(READ_USER_QUERY)) {
+            preStmt.setInt(1, userId);
+            ResultSet rs = preStmt.executeQuery();
+            if (rs.next()) {
+                user.setEmail(rs.getString("email"));
+                user.setUserName(rs.getString("username"));
+                user.setPassword(rs.getString("password"));
+                return user;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
 
 }

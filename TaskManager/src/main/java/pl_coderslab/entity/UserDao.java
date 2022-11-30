08b481e1cf137f3df8ada0129d9b8eb30dfaa08d
Maltaa7 <martajakubowskakra@gmail.com>
2022-11-30 22:10:11 +0100

    DAO read and create

--- TaskManager/src/main/java/pl_coderslab/entity/UserDao.java
+++ TaskManager/src/main/java/pl_coderslab/entity/UserDao.java
@@ -2,6 +2,7 @@ package pl_coderslab.entity;
 
 import org.mindrot.jbcrypt.BCrypt;
 import pl_coderslab.DbUtil;
+
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -9,24 +10,7 @@ import java.sql.SQLException;
 
 public class UserDao {
     private static final String READ_USER_QUERY = "SELECT email, username, password FROM users WHERE id = ?";
-    public User read(int userId) {
-        User user = new User();
-        user.setId(userId);
-        try (Connection connection = DbUtil.connect();
-             PreparedStatement preStmt = connection.prepareStatement(READ_USER_QUERY)) {
-            preStmt.setInt(1, userId);
-            ResultSet rs = preStmt.executeQuery();
-            if (rs.next()) {
-                user.setEmail(rs.getString("email"));
-                user.setUserName(rs.getString("username"));
-                user.setPassword(rs.getString("password"));
-                return user;
-            }
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-        return null;
-    }
+    private static final String CREATE_USER_QUERY = "INSERT INTO users (email, username, password) VALUES (?,?,?)";
 
     public User create(User user) {
         try (Connection connection = DbUtil.connect();
@@ -49,4 +33,25 @@ public class UserDao {
     public String hashPassword(String password) {
         return BCrypt.hashpw(password, BCrypt.gensalt());
     }
+
+    public User read(int userId) {
+        User user = new User();
+        user.setId(userId);
+        try (Connection connection = DbUtil.connect();
+             PreparedStatement preStmt = connection.prepareStatement(READ_USER_QUERY)) {
+            preStmt.setInt(1, userId);
+            ResultSet rs = preStmt.executeQuery();
+            if (rs.next()) {
+                user.setEmail(rs.getString("email"));
+                user.setUserName(rs.getString("username"));
+                user.setPassword(rs.getString("password"));
+                return user;
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+
 }
